96cdcaeba4fe95516a772f10b00be59cbcc414a3
Muhammad Habil Amardias <habilamar@gmail.com>
2024-11-29 16:31:03 +0700

    change download file pipeline

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from upscale_image import upscale_image, load_image, exceed_resolution_threshold
-from display_upscaled import display_upscaled
 from load_model import load_model
 import streamlit as st
+from io import BytesIO
 
 st.set_page_config(layout="centered", 
                    page_title="Image Upscaler")
 
 MAX_SIZE =  1024 * 1024 #1 MB
-extensions = ["png","jpg","jpeg"]
+extensions = ["png","jpg","jpeg","webp"]
 rcan = load_model('src/rcan2.pth')
 
 
@@ -34,17 +34,23 @@ if upload:
         if exceed_resolution_threshold(image):
             st.error('Image resolution is too large, please upload an smaller image')
         else:
-            out_bytes, out_image = upscale_image(image,rcan)
+            out_image = upscale_image(image,rcan)
             filename:str = upload.name
 
             for ext in extensions:
                 if filename.endswith(ext):
                     filename = filename.replace(ext,'')
-            
-            st.image(
-                display_upscaled(image,out_image),
-                channels="BGR"
-            )
+        
+            col1, col2 = st.columns([1,1],vertical_alignment='center')
+            with col1:
+                st.image(image,caption='Before')
+            with col2:
+                st.image(out_image,caption='After Upscaled')
+
+            out_bytes = BytesIO()
+            out_image.save(out_bytes,format='PNG')
+            out_bytes.seek(0)
+
             st.download_button(
                 'Download the Upscaled Image',
                 data=out_bytes,
