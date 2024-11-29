d432705a8c6d2cbed5adf198cc724bd540ceb85f
Muhammad Habil Amardias <habilamar@gmail.com>
2024-11-29 16:30:01 +0700

    preprocessing pipeline update

--- src/upscale_image.py
+++ src/upscale_image.py
@@ -2,33 +2,27 @@ import torch
 import numpy as np
 from model import RCAN
 from torchvision.transforms import v2
-import cv2
+from PIL import Image
 
 transforms = v2.Compose([
+    v2.ToImage(),
     v2.ToDtype(torch.float32,scale=True),
     v2.Normalize(mean=[0.5,0.5,0.5],std=[0.5,0.5,0.5])
 ])
 
 def load_image(upload):
-    file = np.asarray(bytearray(upload.read()),dtype=np.uint8) #convert to bytes
-    image = cv2.imdecode(file,cv2.IMREAD_COLOR)
+    image = Image.open(upload).convert('RGB')
     return image
 
-def exceed_resolution_threshold(image,threshold=350*350):
-    height, width = image.shape[:2]
+def exceed_resolution_threshold(image:Image,threshold=350*350):
+    height, width = image.size
     return height * width > threshold
 
-def upscale_image(image,
+def upscale_image(image:Image,
                   model:RCAN):
     
-    image = np.moveaxis(image,-1,0) #(H,W,C) -> (C,H,W)
-    image = np.expand_dims(image, axis=0) #(C,H,W) -> (1,C,H,W)
-    tensor = torch.tensor(image)
-    tensor = transforms(tensor) #[0,255] -> [-1,1]
-    pred = model.predict(tensor).cpu().numpy().squeeze(axis=0) #generate upscaled image and convert it to numpy
-    pred = np.moveaxis(pred,0,-1) #(C,H,W) -> (H,W,C)
+    tensor = transforms(image).unsqueeze(0) #[0,255] -> [-1,1]
+    pred = model.predict(tensor) #generate upscaled image
     pred = pred*127.5+127.5 #[-1,1] -> [0,255]
-    pred = np.clip(pred,0,255).astype(np.uint8) #convert to unsigned 8-bit integer
-    _, encoded = cv2.imencode('.png',pred)
-
-    return encoded.tobytes(), pred
+    pred = pred.permute(0,2,3,1).squeeze(0).cpu().numpy().astype(np.uint8)
+    return Image.fromarray(pred)
